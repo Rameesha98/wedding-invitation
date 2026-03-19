df2f0896ed34c938ba6ef70b2557bd8baedb78f2
D.M.R.V. Jayawardhana <137770088+Rameesha98@users.noreply.github.com>
2026-03-20 03:02:25 +0530

    Refactor music and countdown functionalities

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 // SELECT MUSIC
 let music = document.getElementById("bgMusic");
 
-
-// ===============================
-// ENTER BUTTON → PLAY MUSIC
-// ===============================
+// ENTER BUTTON
 function enterSite(){
 
   const hero = document.getElementById("hero");
   const main = document.getElementById("main");
 
-  // PLAY MUSIC (NO ERROR)
+  // PLAY MUSIC
   music.muted = false;
   music.volume = 0;
   music.play();
@@ -26,11 +23,6 @@ function enterSite(){
     }
   },200);
 
-  // OPTIONAL VIBRATION (MOBILE)
-  if (navigator.vibrate) {
-    navigator.vibrate(50);
-  }
-
   // ANIMATION
   hero.classList.add("fade-out");
 
@@ -41,18 +33,12 @@ function enterSite(){
   },800);
 }
 
-
-// ===============================
-// MUSIC TOGGLE BUTTON
-// ===============================
+// MUSIC TOGGLE
 function toggleMusic(){
   music.paused ? music.play() : music.pause();
 }
 
-
-// ===============================
-// GOOGLE MAP LINKS
-// ===============================
+// MAP LINKS
 function openPoruwa(){
   window.open("https://maps.app.goo.gl/fx43S51jtNBVq9h79");
 }
@@ -61,10 +47,7 @@ function openWedding(){
   window.open("https://maps.app.goo.gl/GQ1GDR7QPBP6pFQ18");
 }
 
-
-// ===============================
-// COUNTDOWN TIMER
-// ===============================
+// COUNTDOWN
 let countDate = new Date("Sept 17, 2026 10:00:00").getTime();
 
 setInterval(()=>{
